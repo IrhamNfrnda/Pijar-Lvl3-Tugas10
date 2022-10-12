56dca54081dca6a1634aa9afbad83cb33c324774
IrhamNfrnda <inofrianda@gmail.com>
2022-10-12 19:47:36 +0700

    Add CRUD Function

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProdukController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('produk', ProdukController::class);
